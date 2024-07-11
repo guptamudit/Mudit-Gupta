12fed61867e70583104bdd321e1d6d1902840c02
Mudit <muditgupta1711@gmail.com>
2024-07-11 13:28:25 +0530

    "Updated MobileNav component with new UI and added react-icons dependency"

--- mudit-gupta/components/MobileNav.jsx
+++ mudit-gupta/components/MobileNav.jsx
@@ -1,5 +1,66 @@
+"use client";
+
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { CiMenuBurger } from "react-icons/ci";
+
+const links = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "Resume",
+    path: "/resume",
+  },
+
+  {
+    name: "Projects",
+    path: "/projects",
+  },
+  {
+    name: "Contact",
+    path: "/contact",
+  },
+];
+
 const MobileNav = () => {
-  return <div>MobileNav</div>;
+  const pathname = usePathname();
+  return (
+    <Sheet>
+      <SheetTrigger className="flex justify-center items-center">
+        <CiMenuBurger className="text-[32px] text-accent" />
+      </SheetTrigger>
+      <SheetContent className="flex flex-col">
+        {/* logo */}
+        <div className="mt-28 mb-20 text-center text-2xl ">
+          <Link href="/">
+            <h1 className=" text-4xl font-semibold">
+              Mudit <span className="text-accent">.</span>
+            </h1>
+          </Link>
+        </div>
+        {/* nav */}
+        <nav className="flex flex-col justify-center items-center gap-8">
+          {links.map((link, index) => {
+            return (
+              <Link
+                href={link.path}
+                key={index}
+                className={`${
+                  link.path === pathname &&
+                  "text-accent border-b-2 border-accent"
+                }text-xl capitalize hover:text-accent transition-all`}
+              >
+                {link.name}
+              </Link>
+            );
+          })}
+        </nav>
+      </SheetContent>
+    </Sheet>
+  );
 };
 
 export default MobileNav;
