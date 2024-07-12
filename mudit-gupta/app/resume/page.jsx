4fe6b4ddac1d7e2353e04f04360d69ec9ee1ae96
Mudit <muditgupta1711@gmail.com>
2024-07-12 23:32:03 +0530

    "Updated About Me section, changed animation initial opacity, and refactored Skills and About tabs content"

--- mudit-gupta/app/resume/page.jsx
+++ mudit-gupta/app/resume/page.jsx
@@ -18,7 +18,7 @@ import { TfiMicrosoftAlt } from "react-icons/tfi";
 const about = {
   title: "About Me",
   description:
-    "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam eveniet debitis ut dolorum aperiam magnam aut quibusdam similique dicta sed",
+    "A Passionate B-tech student, constantly seeking new opportunities to grow and contribute in tech field. My aim is to acquire new skills to make up with the constantly growing market. I'm excited to connect with like-minded individuals and collaborate on projects.",
   info: [
     {
       fieldName: "Name",
@@ -41,7 +41,7 @@ const about = {
       fieldValue: "English, Hindi",
     },
     {
-      fieldName: "FreeOrNot",
+      fieldName: "Availability",
       fieldValue: "Avialabe to Work",
     },
   ],
@@ -182,7 +182,7 @@ import { Item } from "@radix-ui/react-select";
 const Resume = () => {
   return (
     <motion.div
-      nitial={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
       animate={{
         opacity: 1,
         transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
@@ -245,7 +245,7 @@ const Resume = () => {
                       return (
                         <li
                           key={index}
-                          className="bg-[#232329] h-[140px] xl:w-[350px] py-6 px-5 rounded-xl flex flex-col justify-center items-center lg:items-start"
+                          className="bg-[#232329] h-[140px] lg:w-[350px] py-6 px-5 rounded-xl flex flex-col justify-center items-center lg:items-start"
                         >
                           <span className="text-accent ">
                             {item.institution}
@@ -264,12 +264,65 @@ const Resume = () => {
               </div>
             </TabsContent>
             {/* skills */}
-            <TabsContent value="skills" className="w-full">
-              skills
+            <TabsContent value="skills" className="w-full h-full">
+              <div className="flex flex-col gap-[30px]">
+                <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                  <h3 className="text-4xl font-bold">{skills.title}</h3>
+                  <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                    {skills.description}
+                  </p>
+                </div>
+                <ScrollArea className="h-[300px]">
+                  <ul className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-[30px]  ">
+                    {skills.skillList.map((item, index) => {
+                      return (
+                        <li key={index}>
+                          <TooltipProvider delayDuration={80}>
+                            <Tooltip>
+                              <TooltipTrigger className="w-full h-[150px] bg-[#232329] rounded-xl flex flex-col justify-center items-center group">
+                                <div className="text-6xl group-hover:text-accent transition-all duration-300">
+                                  {item.icon}
+                                </div>
+                              </TooltipTrigger>
+                              <TooltipContent>
+                                <p>{item.name}</p>
+                              </TooltipContent>
+                            </Tooltip>
+                          </TooltipProvider>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div>
             </TabsContent>
             {/* about */}
-            <TabsContent value="about" className="w-full">
-              about
+            <TabsContent
+              value="about"
+              className="w-full text-center xl:text-left "
+            >
+              <div className="flex flex-col gap-[30px]">
+                <h3 className="text-4xl font-bold">{about.title}</h3>
+                <p className="max-w-[700px] text-white/60 mx-auto xl:mx-0">
+                  {about.description}
+                </p>
+                <ul className="grid grid-cols-1  xl:grid-cols-2 gap-y-6 max-w-[720px] max-auto xl:mx-0">
+                  {about.info.map((item, index) => {
+                    return (
+                      <li
+                        key={index}
+                        className="flex items-center justify-center xl:justify-start gap-4"
+                      >
+                        <span className="text-white/60">
+                          {item.fieldName}{" "}
+                          <span className="text-accent">:</span>
+                        </span>
+                        <span className="">{item.fieldValue}</span>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
             </TabsContent>
           </div>
         </Tabs>
